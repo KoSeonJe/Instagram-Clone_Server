4311d9dee5fff656a8722d2567b50b204ceafafa
koseonje <kosunje1455@navet.com>
2024-02-28 03:02:06 +0900

    feat: Redis관련 설정 클래스 구현

---
+++ src/main/java/com/example/instagrambe/common/config/RedisConfig.java
@@ -0,0 +1,38 @@
+package com.example.instagrambe.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@RequiredArgsConstructor
+public class RedisConfig {
+
+  @Value("${redis.host}")
+  private String host;
+
+  @Value("${redis.port}")
+  private int port;
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory(host, port);
+  }
+
+  @Bean
+  public RedisTemplate<String, String> redisTemplate() {
+    RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new StringRedisSerializer());
+    redisTemplate.setEnableTransactionSupport(true);
+    return redisTemplate;
+  }
+}
