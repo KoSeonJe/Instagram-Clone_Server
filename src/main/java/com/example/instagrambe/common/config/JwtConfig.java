9ee4fbd9e8ea6054f173b8ba290674f276b3c3e4
koseonje <kosunje1344@naver.com>
2024-03-05 16:37:45 +0900

    Refactor: 클래스명 컨벤션 지키기

--- src/main/java/com/example/instagrambe/common/config/JwtConfig.java
+++ src/main/java/com/example/instagrambe/common/config/JwtConfig.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class jwtConfig {
+public class JwtConfig {
 
   private final TokenRepository tokenRepository;
   private final JwtProperties jwtProperties;
