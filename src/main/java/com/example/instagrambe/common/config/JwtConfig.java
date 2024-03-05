32e258b1daf70695ae04b95c95a90a01034a6948
koseonje <kosunje1344@naver.com>
2024-03-05 18:21:39 +0900

    Refactor: 필요 없는 Bean생성 제거

--- src/main/java/com/example/instagrambe/common/config/JwtConfig.java
+++ src/main/java/com/example/instagrambe/common/config/JwtConfig.java
@@ -4,7 +4,6 @@ import com.example.instagrambe.domain.auth.jwt.constant.JwtProperties;
 import com.example.instagrambe.domain.auth.jwt.filter.JwtAuthenticationFilter;
 import com.example.instagrambe.domain.auth.jwt.filter.JwtExceptionFilter;
 import com.example.instagrambe.domain.auth.jwt.repository.TokenRepository;
-import com.example.instagrambe.domain.auth.jwt.service.JwtProvider;
 import com.example.instagrambe.domain.auth.jwt.service.JwtService;
 import com.example.instagrambe.domain.member.service.MemberService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,27 +17,18 @@ public class JwtConfig {
 
   private final TokenRepository tokenRepository;
   private final JwtProperties jwtProperties;
+  private final JwtService jwtService;
   private final MemberService memberService;
   private final ObjectMapper objectMapper;
 
 
   @Bean
   public JwtAuthenticationFilter jwtAuthenticationProcessingFilter() {
-    return new JwtAuthenticationFilter(jwtService(), memberService, jwtProperties);
+    return new JwtAuthenticationFilter(jwtService, jwtProperties);
   }
 
   @Bean
   public JwtExceptionFilter jwtExceptionFilter() {
     return new JwtExceptionFilter(objectMapper);
-  }
-
-  @Bean
-  public JwtService jwtService() {
-    return new JwtService(tokenRepository, jwtProvider(), jwtProperties);
-  }
-
-  @Bean
-  public JwtProvider jwtProvider() {
-    return new JwtProvider(tokenRepository, jwtProperties);
   }
 }
